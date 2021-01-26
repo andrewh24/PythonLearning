059291f9feed88b003df913d078b098597e8e76c
Ondřej Horka <horka.ondrej@gmail.com>
2021-01-26 19:59:53 +0100

    getting friends from twitter api

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-print('Hello World!');
-print('Hello World!');
+import twitter
+from t import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+
+# Create an Api instance.
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+                  consumer_secret=CONSUMER_SECRET,
+                  access_token_key=ACCESS_TOKEN_KEY,
+                  access_token_secret=ACCESS_TOKEN_SECRET)
+
+users = api.GetFriends()
+
+print([u.screen_name for u in users])
