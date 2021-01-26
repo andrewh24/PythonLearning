ac0fa975b6ce214f54f926384124adf67d1b08a6
Ondřej Horka <horka.ondrej@gmail.com>
2021-01-26 20:28:08 +0100

    getting timeline and writing to file as json

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 import twitter
 from t import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
@@ -7,6 +10,13 @@ api = twitter.Api(consumer_key=CONSUMER_KEY,
                   access_token_key=ACCESS_TOKEN_KEY,
                   access_token_secret=ACCESS_TOKEN_SECRET)
 
-users = api.GetFriends()
+# users = api.GetFriends()
+tweets = api.GetUserTimeline(user_id=196700354, count=200)
 
-print([u.screen_name for u in users])
+with open("tweets.json", "w") as f:
+    for tweet in tweets:
+        # f.write(json.dumps(tweet.text))
+        f.write(json.dumps(tweet._json))
+        f.write("\n")
+
+# print([u.screen_name for u in users])
