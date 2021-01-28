fed3b24d94a68eb588720b00b0baced8adc5776e
andrewh24 <horka.ondrej@gmail.com>
2021-01-28 11:45:32 +0100

    class, method testing

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
 import json
 import sys
+import os
+import doctest
+import csv
+from datetime import date
 
 import twitter
 from t import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
+class MyClass:
+    """A simple testing class with two parameters."""
+    def __init__(self, str):
+        self.strg = str
+    
+    def rev(self, s):
+        """A simple testing method.
+        >>> x = MyClass('ahoj')
+        >>> x.rev('ahoj')
+        joha
+        """
+        for i in range(len(s)-1, -1, -1):
+            print(s[i], end='')
+
+doctest.testmod()
 # Create an Api instance.
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=ACCESS_TOKEN_KEY,
-                  access_token_secret=ACCESS_TOKEN_SECRET)
+# api = twitter.Api(consumer_key=CONSUMER_KEY,
+#                   consumer_secret=CONSUMER_SECRET,
+#                   access_token_key=ACCESS_TOKEN_KEY,
+#                   access_token_secret=ACCESS_TOKEN_SECRET)
 
 # users = api.GetFriends()
-tweets = api.GetUserTimeline(user_id=196700354, count=200)
+# tweets = api.GetUserTimeline(user_id=196700354, count=200)
 
-with open("tweets.json", "w") as f:
-    for tweet in tweets:
-        # f.write(json.dumps(tweet.text))
-        f.write(json.dumps(tweet._json))
-        f.write("\n")
+# with open("tweets.json", "w") as f:
+#     for tweet in tweets:
+#         # f.write(json.dumps(tweet.text))
+#         f.write(json.dumps(tweet._json))
+#         f.write("\n")
 
 # print([u.screen_name for u in users])
